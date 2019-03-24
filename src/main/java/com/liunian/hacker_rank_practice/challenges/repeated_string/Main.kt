7493f04cda301af2decd0e150ad74a830d27f9d6
Nian Liu <nliu@bol.com>
2019-03-24 17:11:48 +0100

    repeated string

---
+++ src/main/java/com/liunian/hacker_rank_practice/challenges/repeated_string/Main.kt
@@ -0,0 +1,21 @@
+package com.liunian.hacker_rank_practice.challenges.repeated_string
+
+import java.util.*
+
+// Complete the repeatedString function below.
+fun repeatedString(s: String, n: Long): Long {
+    val countA = { string: String -> string.count { it == 'a' } }
+    return n / s.length * countA(s) + countA(s.substring(0, (n % s.length).toInt()))
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val s = scan.nextLine()
+
+    val n = scan.nextLine().trim().toLong()
+
+    val result = repeatedString(s, n)
+
+    println(result)
+}
