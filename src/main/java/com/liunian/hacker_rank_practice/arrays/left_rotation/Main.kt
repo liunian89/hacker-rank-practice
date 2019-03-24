3d314cd7eaac91452aafc8a1ab470e9a836e154d
Nian Liu <nliu@bol.com>
2019-03-24 18:14:19 +0100

    copy lulu's approach

--- src/main/java/com/liunian/hacker_rank_practice/arrays/left_rotation/Main.kt
+++ src/main/java/com/liunian/hacker_rank_practice/arrays/left_rotation/Main.kt
@@ -16,6 +16,11 @@ private fun indexOf(index: Int, size: Int): Int {
     }
 }
 
+fun rotLeftByLulu(a: Array<Int>, d: Int): Array<Int> {
+    val right = a.slice(d until a.size)
+    val left = a.slice(0 until d)
+    return (right + left).toTypedArray()
+}
 
 fun main(args: Array<String>) {
     val scan = Scanner(System.`in`)
