6391edab639d553a761d75528cde3084214a8941
Nian Liu <nliu@bol.com>
2019-03-24 18:12:48 +0100

    left rotation

---
+++ src/main/java/com/liunian/hacker_rank_practice/arrays/left_rotation/Main.kt
@@ -0,0 +1,34 @@
+package com.liunian.hacker_rank_practice.arrays.left_rotation
+
+import java.util.*
+
+// Complete the rotLeft function below.
+fun rotLeft(a: Array<Int>, d: Int): Array<Int> {
+    val array = Array(a.size) { 0 }
+    a.forEachIndexed { index, i -> array[indexOf(index - d, a.size)] = i }
+    return array
+}
+
+private fun indexOf(index: Int, size: Int): Int {
+    return when {
+        index >= 0 -> index
+        else -> size + index
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val nd = scan.nextLine().split(" ")
+
+    val n = nd[0].trim().toInt()
+
+    val d = nd[1].trim().toInt()
+
+    val a = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+
+    val result = rotLeft(a, d)
+
+    println(result.joinToString(" "))
+}
