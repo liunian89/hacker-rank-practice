67a59b47c45dcaf910ba311655bd661f03f6a582
Nian Liu <nliu@bol.com>
2019-04-10 23:22:14 +0200

    swap sort

--- src/main/java/com/liunian/hacker_rank_practice/arrays/minimum_swaps/Main.kt
+++ src/main/java/com/liunian/hacker_rank_practice/arrays/minimum_swaps/Main.kt
@@ -4,40 +4,18 @@ import java.util.*
 import kotlin.math.absoluteValue
 
 fun minimumSwaps(arr: Array<Int>): Int {
-
+    var i = 0
     var count = 0
-    var diff = arr.diff()
-    while (true) {
-
-        val bestImprovement = diff.values.map { it.distanceAfterSwap.absoluteValue }.min()
-        if (bestImprovement == 0) {
-            break
+    while (i < arr.size) {
+        if (arr[i] == i + 1) {
+            i++
+        } else {
+            arr[i] = arr[arr[i] - 1].also { arr[arr[i] - 1] = arr[i] }
+            count++
         }
-        val one = diff.values.first { it.distanceAfterSwap.absoluteValue == bestImprovement }
-        val swapped = arr[one.value -1]
-        arr[one.value - 1] = one.value.also { arr[one.index] = arr[one.value - 1] }
-        count++
-        diff.remove(one.value)
-        val element = Element( one.index, swapped, diff[arr[one.index]]!!.distanceAfterSwap + one.distanceAfterSwap)
-        diff[arr[one.index]] = element
     }
-
     return count
 }
-
-private fun Array<Int>.diff(): MutableMap<Int, Element> {
-
-    val a = this.mapIndexed { index, value -> Triple(index, value, index - value + 1) }
-    val b = a.map { Element(it.first, it.second, it.third + it.second - this[it.second - 1]) }
-            .filter { it.value != it.index + 1 }
-            .map { it.value to it }
-            .toMap()
-            .toMutableMap()
-    return b
-}
-
-
-data class Element(val index: Int, val value: Int, val distanceAfterSwap: Int)
 
 fun main(args: Array<String>) {
     val scan = Scanner(System.`in`)
