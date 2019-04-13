3fbffdcfd9ba277f2b6485274894b598c081f589
Nian Liu <nliu@bol.com>
2019-04-13 21:34:28 +0200

    ransom note

---
+++ src/main/java/com/liunian/hacker_rank_practice/hashmap_dict/ransom_note/Main.kt
@@ -0,0 +1,29 @@
+package com.liunian.hacker_rank_practice.hashmap_dict.ransom_note
+
+import java.util.*
+
+// Complete the checkMagazine function below.
+fun checkMagazine(magazine: Array<String>, note: Array<String>): Unit {
+    val noteDict = note.groupingBy { it }.eachCount()
+    val magazineDict = magazine.groupingBy { it }.eachCount()
+
+    val isPossible = !noteDict.entries.any { it.value > magazineDict[it.key] ?: 0 }
+
+    println(if (isPossible) "Yes" else "No")
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val mn = scan.nextLine().split(" ")
+
+    val m = mn[0].trim().toInt()
+
+    val n = mn[1].trim().toInt()
+
+    val magazine = scan.nextLine().split(" ").toTypedArray()
+
+    val note = scan.nextLine().split(" ").toTypedArray()
+
+    checkMagazine(magazine, note)
+}
